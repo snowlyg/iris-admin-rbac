6928cc32a099a0f7562dac7a4d71d9d30dfe6271
snwolyg <569616226@qq.com>
2022-01-02 17:03:59 +0800

    update doc

--- doc.go
+++ doc.go
@@ -1,4 +1,5 @@
 /**
+git tag -d v0.0.1-alpha1 v0.0.1-alpha2 v0.0.1-alpha3 v0.0.1-alpha4 v0.0.1-alpha5 v0.0.1-alpha6 v0.0.1-alpha7 v0.0.1-alpha8 v0.0.1-alpha9 v0.0.1-alpha10 v0.0.1-alpha11 v0.0.1-alpha12 v0.0.1-alpha13 v0.0.1-alpha14 v0.0.1-alpha15 v0.0.1-alpha16 v0.0.1-alpha17 v0.0.1-alpha18 v0.0.1-alpha19
 <h1 align="center">IrisAdminRbac</h1>
 
 [IrisAdminRbac](https://www.github.com/snowlyg/iris-admin-rbac) 项目为一个权鉴模块插件,可以为 [IrisAdmin](https://www.github.com/snowlyg/iris-admin) 项目快速集成权鉴管理API.
